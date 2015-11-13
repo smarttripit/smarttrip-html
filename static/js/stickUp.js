343a060ea4b071f149a8802f482b24d68b60999f
panpan040828 <875371217@qq.com>
2015-11-13 10:40:27 +0800

    解决某个景点页面悬浮在顶部导航栏点击出错问题
    解决某个景点页面悬浮在顶部导航栏点击出错问题

--- static/js/stickUp.js
+++ static/js/stickUp.js
@@ -5,6 +5,7 @@ function($) {
 		var contentButton = [];
 		var contentTop = [];
 		var content = [];
+		var content1 = [];
 		var lastScrollTop = 0;
 		var scrollDir = '';
 		var itemClass = '';
@@ -14,21 +15,19 @@ function($) {
 		var stickyMarginB = 0;
 		var currentMarginT = 0;
 		var topMargin = 0;
-		//判断垂直滚动条是像上还是像下滚动
 		$(window).scroll(function(event){
-   			var st = $(this).scrollTop();//滚动条相对于其顶部的偏移
+   			var st = $(this).scrollTop();
    			if (st > lastScrollTop){
        			scrollDir = 'down';
-       			console.log('down');
+       			//console.log('down');
    			} else {
       			scrollDir = 'up';
-      			console.log('up');
+      			//console.log('up');
    			}
   			lastScrollTop = st;
 		});
-         //stickUp是函数名
 		$.fn.stickUp = function( options ) {
-			// adding a class to users div 
+			// adding a class to users div
 			$(this).addClass('stuckMenu');
         	//getting options
         	var objn = 0;
@@ -36,6 +35,7 @@ function($) {
 	        	for(var o in options.parts) {
 	        		if (options.parts.hasOwnProperty(o)){
 	        			content[objn] = options.parts[objn];
+	        			content1[objn] = options.parts1[objn];
 	        			objn++;
 	        		}
 	        	}
@@ -71,19 +71,17 @@ function($) {
 		}
 		$(document).on('scroll', function() {
 			varscroll = parseInt($(document).scrollTop());
-			console.log(varscroll); //
+			//console.log(varscroll);
 			if(menuSize != null){
 				for(var i=0;i < menuSize;i++)
 				{
-					contentTop[i] = $('#'+content[i]+'').offset().top;//每一块content相对于浏览器上端的距离
-					console.info(contentTop);
+					contentTop[i] = $('#'+content[i]+'').offset().top;
+					//console.log(contentTop);
 					function bottomView(i) {
-						contentView = $('#'+content[i]+'').height()*.4;//每一块content的高度乘以0.4
+						contentView = $('#'+content1[i]+'').height()*.2;
 						testView = contentTop[i] - contentView;
-						//
-						console.log(contentView);//
-						console.error(testView);//
-						//console.log(varscroll);
+						//console.log(contentView);
+						//console.error(testView);
 						if(varscroll > testView){
 							$('.'+itemClass).removeClass(itemHover);
 							$('.'+itemClass+':eq('+i+')').addClass(itemHover);
@@ -100,7 +98,6 @@ function($) {
 						bottomView(i);
 					}
 				}
-				
 			}
 
 
