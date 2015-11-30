4f973c27a56da899186306fec1be04cee1a4de21
songjie <songjiesdnu@163.com>
2015-12-01 00:27:54 +0800

    实现无刷新分页组件

---
+++ static/js/pagination/st-paginator.js
@@ -0,0 +1,48 @@
+﻿// 无刷新分页
+function pagination(paginationOptions) {
+	var domId = paginationOptions.domId;
+	var url = paginationOptions.url;
+	var page = paginationOptions.page;
+	var rows = paginationOptions.rows;
+	paginationOptions.queryParams.page = paginationOptions.page;
+	paginationOptions.queryParams.rows = paginationOptions.rows;
+	var queryParams = paginationOptions.queryParams;
+	$.ajax({
+		type : "GET",
+		url : url,
+		cache : false,
+		data : queryParams,
+		dataType : "json",
+		success : function(result) {
+			var data = result.data;
+			var dataList = data.rows;
+			paginationOptions.dealData(dataList);
+			var pages = Math.ceil(data.total / rows); //这里data里面有数据总量
+			var element = $('#' + domId); //对应下面ul的ID
+			var options = {
+				bootstrapMajorVersion : 3,
+				currentPage : page, //当前页面
+				numberOfPages : 5, //一页显示几个按钮（ 在ul里面生成5个li）
+				totalPages : pages, //总页数
+				tooltipTitles : function(type, page, current) {
+					if (type == 'first') {
+						return "首页";
+					} else if (type == 'prev') {
+						return "上一页";
+					} else if (type == 'page') {
+						return "跳转到第" + page + "页";
+					} else if (type == 'next') {
+						return "下一页";
+					} else if (type == 'last') {
+						return "尾页";
+					}
+				},
+				onPageClicked : function(event, originalEvent, type, page) {
+					paginationOptions.page = page;
+					pagination(paginationOptions);
+				}
+			}
+			element.bootstrapPaginator(options);
+		}
+	});
+}
