22ec3168cc2ed4968e626b7f53d64ec024304cad
panpan040828 <875371217@qq.com>
2015-12-17 18:00:48 +0800

    景点详情页 图片集后台数据对接
    景点详情页 图片集后台数据对接

--- static/js/smoothproducts.js
+++ static/js/smoothproducts.js
@@ -2,6 +2,7 @@
 $('.sp-wrap').append('<div class="sp-large"></div><div class="sp-thumbs sp-tb-active"></div>');//在.sp-wrap这个div后面加2个div
 $('.sp-wrap a').appendTo('.sp-thumbs');
 $('.sp-thumbs a:first').addClass('sp-current').clone().removeClass('sp-current').appendTo('.sp-large');
+
 $('.sp-wrap').css('display', 'inline-block');
 var slideTiming = 300;
 var maxWidth = $('.sp-large img').width();
@@ -37,81 +38,5 @@ $(document.body).on('click', '.sp-thumbs' ,function(event){
     event.preventDefault();
 });
 
-// Zoom In
-/*$(document.body).on('click', '.sp-large a' ,function(event){
-    var largeUrl = $(this).attr('href');
-    $('.sp-large').append('<div class="sp-zoom"><img src="' + largeUrl + '"/></div>');
-    $('.sp-zoom').fadeIn();
-    $('.sp-large').css({
-        left: 0,
-        top: 0
-    });
-    
-    event.preventDefault();
-});*/
 
-// Panning zoomed image
-/*$(document).ready(function () {
-    $('.sp-large').mousemove(function (e) {
-        var viewWidth = $('.sp-large').width();
-        var viewHeight = $('.sp-large').height();
-        var largeWidth = $('.sp-zoom').width();
-        var largeHeight = $('.sp-zoom').height();
-        var parentOffset = $(this).parent().offset();
-        var relativeXPosition = (e.pageX - parentOffset.left);
-        var relativeYPosition = (e.pageY - parentOffset.top);
-        var moveX = Math.floor((relativeXPosition * (viewWidth - largeWidth) / viewWidth));
-        var moveY = Math.floor((relativeYPosition * (viewHeight - largeHeight) / viewHeight));
 
-        $('.sp-zoom').css({
-            left: moveX,
-            top: moveY
-        });
-
-    }).mouseout(function () {
-        //
-    });
-});*/
-
-// Panning on mobile devices using Gyroscope. Aw Yeah.
-/*window.ondeviceorientation = function(event) {
-        var viewWidth = $('.sp-large').width();
-        var viewHeight = $('.sp-large').height();
-        var largeWidth = $('.sp-zoom').width();
-        var largeHeight = $('.sp-zoom').height();
-        var pitchInit = Math.floor(event.beta)+90;
-        var pitch = Math.floor((-(largeHeight - viewHeight)/(150-100))*(pitchInit-100));
-        var rollInit = Math.floor(event.gamma)+180;
-        var roll = Math.floor((-(largeWidth - viewWidth)/(200-160))*(rollInit-160));
-
-        // Prevent moving too far on X axis
-        if (roll < -(largeWidth - viewWidth)) {
-            //
-        } else if(roll > 0){
-            //
-        } else {
-            $('.sp-zoom').css({
-                left : roll + 'px'
-            }); 
-        }
-
-        // Prevent moving too far on Y axis
-        if (pitch < -(largeHeight - viewHeight)) {
-            //
-        } else if(pitch > 0){
-            //
-        } else {
-            $('.sp-zoom').css({
-                top : pitch + 'px'
-            }); 
-        }
-    }
-*/
-
-// Zoom out
-/*$(document.body).on('click', '.sp-zoom' ,function(event){
-    $(this).fadeOut(function () {
-        $(this).remove();
-    });
-});
-*/
