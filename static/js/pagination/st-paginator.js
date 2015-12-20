6cf734dedf915bcbce523a48970d6568e55fda27
songjie <songjiesdnu@163.com>
2015-12-20 17:36:31 +0800

    解决无刷新分页组件在没有数据情况下的一个outofrange异常

--- static/js/pagination/st-paginator.js
+++ static/js/pagination/st-paginator.js
@@ -17,32 +17,34 @@ function pagination(paginationOptions) {
 			var data = result.data;
 			var dataList = data.rows;
 			paginationOptions.dealData(dataList);
-			var pages = Math.ceil(data.total / rows); //这里data里面有数据总量
 			var element = $('#' + domId); //对应下面ul的ID
-			var options = {
-				bootstrapMajorVersion : 3,
-				currentPage : page, //当前页面
-				numberOfPages : 5, //一页显示几个按钮（ 在ul里面生成5个li）
-				totalPages : pages, //总页数
-				tooltipTitles : function(type, page, current) {
-					if (type == 'first') {
-						return "首页";
-					} else if (type == 'prev') {
-						return "上一页";
-					} else if (type == 'page') {
-						return "跳转到第" + page + "页";
-					} else if (type == 'next') {
-						return "下一页";
-					} else if (type == 'last') {
-						return "尾页";
+			var pages = Math.ceil(data.total / rows); //这里data里面有数据总量
+			if(pages != 0){//没有数据，则不初始化分页条
+				var options = {
+						bootstrapMajorVersion : 3,
+						currentPage : page, //当前页面
+						numberOfPages : 5, //一页显示几个按钮（ 在ul里面生成5个li）
+						totalPages : pages, //总页数
+						tooltipTitles : function(type, page, current) {
+							if (type == 'first') {
+								return "首页";
+							} else if (type == 'prev') {
+								return "上一页";
+							} else if (type == 'page') {
+								return "跳转到第" + page + "页";
+							} else if (type == 'next') {
+								return "下一页";
+							} else if (type == 'last') {
+								return "尾页";
+							}
+						},
+						onPageClicked : function(event, originalEvent, type, page) {
+							paginationOptions.page = page;
+							pagination(paginationOptions);
+						}
 					}
-				},
-				onPageClicked : function(event, originalEvent, type, page) {
-					paginationOptions.page = page;
-					pagination(paginationOptions);
-				}
+					element.bootstrapPaginator(options);
 			}
-			element.bootstrapPaginator(options);
 		}
 	});
 }
