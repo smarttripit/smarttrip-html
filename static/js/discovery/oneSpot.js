1422a0f4c51c6044cf775db9f2cb5901b3df75ce
panpan040828 <875371217@qq.com>
2015-12-20 22:35:45 +0800

    调整样式
    调整样式

--- static/js/discovery/oneSpot.js
+++ static/js/discovery/oneSpot.js
@@ -1,14 +1,4 @@
-    (function($){
-          $.getUrlParam
-            = function(name)
-                     {
-                      var reg = new RegExp("(^|&)"+name +"=([^&]*)(&|$)");
-                      var r = window.location.search.substr(1).match(reg);
-                      if (r!=null) 
-                        return unescape(r[2]); 
-                      return null;
-                     }
-    })(jQuery);
+
     var spotId = $.getUrlParam('spotId');
     var productId = $.getUrlParam('productId');
 
@@ -137,6 +127,61 @@
     });
 
 
+       //好评差评中评
+       $("#good_bad_comment ul li a").click(function(){if($(this).has(".defined_active")){ 
+       $(this).addClass("defined_active").parent("li").siblings("li").find("a").removeClass("defined_active");}}) 
 
+          $(document).ready(function() {
+     // 加载发现北京评论列表
+     var paginationOptions = {
+       domId : "pager2",
+       productType:"03",
+       url : "/comment/getByPage?productId="+productId,
+       page : 1,
+       rows : 6,
+       queryParams : {score:""},
+       dealData : function(dataList){
+         var len = dataList.length;
+         if(len > 0){
+           for(var i=0; i<len; i++){
+             var row = dataList[i];
+             $("#comment_"+i).css("display","block" );
+             //console.log(row.routeId);            
+             $("#comment_id_"+i).text(row.commentId);//
+             $("#comment_content_"+i).text(row.content);            
+             $("#comment_score_"+i).text(row.score);//
+             $("#comment_commentTime_"+i).text(row.commentTime);
+             $("#comment_visitorName_"+i).text(row.visitorName);
+             $("#comment_visitorId_"+i).text(row.visitorId);//
+             var images = row.images;
+             var len1=images.length;
+             $("#comment_imgNum_"+i).text(len1);
+             var imagesHref2=[];
+
+             if(images.length > 0){
+               for(var j=0; j<images.length; j++){
+                 var img = images[j];
+                 var obj=new Object();
+                 $("#comment_images_"+i+j).css("display","block" );
+                 $("#comment_images_"+i+j).attr("src",img.imageURL);
+                 obj.href= img.imageURL;
+                 imagesHref2.push(obj);//按顺序依次push进去 
+               }
+             }
+             console.log(imagesHref2);
+             $('.fancybox').fancybox();
+             $(".fancybox-manual-a").click(function() {
+                $.fancybox.open(imagesHref2,
+                  {
+                    helpers : { thumbs : {width: 75,height: 50}
+                  }
+                });
+             });
+           }
+         }
+       }
+     }
+     pagination(paginationOptions);
+    });
 
 
