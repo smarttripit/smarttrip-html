157da9584a5f6f4d85c23729ce07a658785b2b1e
Joyling <cgl85by@126.com>
2015-12-20 18:08:51 +0800

    客栈详情页面
    客栈详情页面

--- static/js/hotel/oneHotel.js
+++ static/js/hotel/oneHotel.js
@@ -4,22 +4,26 @@
 
           parts: {                         
             0:'hotel_style',
-            1: 'basic_information',
-            2: 'traffic_guidance',
-            3: 'zhouzhou_tips',
-            4: 'travel_details',
-            5: 'zhouzhou_comment',
-            6:'similar_comment'
+            1: 'hotel_address',
+            2: 'hotel_information',
+            3: 'hotel_photos',
+            4: 'hotel_policy',
+            5: 'hotel_infrastructure',
+            6: 'neighbor_infrastructure',
+            7: 'zhouzhou_commend',
+            8: 'zhouzhou_comment'
             
           },
           parts1: {                         
             0:'hotel_style1',
-            1: 'basic_information1',
-            2: 'traffic_guidance1',
-            3: 'zhouzhou_tips1',
-            4: 'travel_details1',
-            5: 'zhouzhou_comment1',
-            6:'similar_comment1'
+            1: 'hotel_address1',
+            2: 'hotel_information1',
+            3: 'hotel_photos1',
+            4: 'hotel_policy1',
+            5: 'hotel_infrastructure1',
+            6: 'neighbor_infrastructure1',
+            7: 'zhouzhou_commend1',
+            8: 'zhouzhou_comment1'
           },
           itemClass: 'menuItem',
           itemHover: 'active',
