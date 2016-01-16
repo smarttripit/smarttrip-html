699f22e20ce3a4807b7602ad36a0dcfc5b40ffe5
panpan040828 <875371217@qq.com>
2016-01-16 18:04:40 +0800

    冲突
    冲突

--- static/js/routeOrder/createOrder.js
+++ static/js/routeOrder/createOrder.js
@@ -5,13 +5,13 @@ var maleCount = $.getUrlParam('maleCount');
 var startDate = $.getUrlParam('startDate');
 var price = $.getUrlParam('price');
 
-$(document).ready( function() {
-	//初始化订单信息
+$(document).ready(function() {
+	// 初始化订单信息
 	initOrderInfo();
 });
 
-//初始化订单信息
-function initOrderInfo(){
+// 初始化订单信息
+function initOrderInfo() {
 	var totalCount = parseInt(femaleCount) + parseInt(maleCount);
 	var totalPrice = parseFloat(price) * totalCount;
 	$('#femaleCount').text(femaleCount);
@@ -20,6 +20,7 @@ function initOrderInfo(){
 	$('#price').text(price);
 	$('#totalCount').text(totalCount);
 	$('#totalPrice').text(totalPrice);
+<<<<<<< HEAD
   var totalCount = parseInt(femaleCount) + parseInt(maleCount);
   console.log(totalCount);
   for(i=0;i<totalCount;i++)
@@ -65,25 +66,83 @@ function initOrderInfo(){
                 '</div>\n');
     //console.log("1");
   }
+=======
+	var totalCount = parseInt(femaleCount) + parseInt(maleCount);
+	console.log(totalCount);
+	for (i = 0; i < totalCount; i++) {
+		var j = i + 1;
+		var tourist_name = [];
+		var tourist_IDCardNo = [];
+		var tourist_mobileNo = [];
+		var tourist_weixinNo = [];
+		tourist_name[i] = "tourist_name_" + i;
+		tourist_IDCardNo[i] = "tourist_IDCardNo_" + i;
+		tourist_mobileNo[i] = "tourist_mobileNo_" + i;
+		tourist_weixinNo[i] = "tourist_weixinNo_" + i;
+
+		$("#tourist_info")
+				.next()
+				.append(
+						'<div class="col-xs-12 col-sm-12 col-md-4 row_bottom_space1">\n'
+								+ '<div class="col-xs-12 col-sm-12 col-md-12 ">\n'
+								+ '<p><b class="font_size14 row_bottom_space "><i class="fa fa-user"></i>&nbsp;游客\n'
+								+ j
+								+ '信息</b></p>\n'
+								+ '</div>\n'
+								+ '<div class="form-group  ">\n'
+								+ '<label for="" class="col-xs-4 col-sm-3 col-md-4 control-label pr0 mt5 ">姓名：</label>\n'
+								+ '<div class="col-xs-8 col-sm-9 col-md-8 pl0 ">\n'
+								+ '<input  type="text" class="form-control" id='
+								+ tourist_name[i]
+								+ ' placeholder="必填">\n'
+								+ '</div>\n'
+								+ '</div>\n'
+								+ '<div class="form-group ">\n'
+								+ '<label for="" class="col-xs-4 col-sm-3 col-md-4 control-label pr0 mt5 mt10">手机号：</label>\n'
+								+ '<div class="col-xs-8 col-sm-9 col-md-8 pl0 mt10">\n'
+								+ '<input  type="text" class="form-control" id='
+								+ tourist_IDCardNo[i]
+								+ ' placeholder="必填">\n'
+								+ '</div>\n'
+								+ '</div>\n'
+								+ '<div class="form-group ">\n'
+								+ '<label for="" class="col-xs-4 col-sm-3 col-md-4 control-label pr0 mt5 mt10">身份证号：</label>\n'
+								+ '<div class="col-xs-8 col-sm-9 col-md-8 pl0 mt10">\n'
+								+ '<input  type="text" class="form-control" id='
+								+ tourist_mobileNo[i]
+								+ ' placeholder="必填">\n'
+								+ '</div>\n'
+								+ '</div>\n'
+								+ '<div class="form-group ">\n'
+								+ '<label for="" class="col-xs-4 col-sm-3 col-md-4 control-label pr0 mt5 mt10">微信号：</label>\n'
+								+ '<div class="col-xs-8 col-sm-9 col-md-8 pl0 mt10">\n'
+								+ '<input  type="text" class="form-control" id='
+								+ tourist_weixinNo[i]
+								+ '  placeholder="选填">\n'
+								+ '</div>\n' + '</div>\n' + '</div>\n');
+		// console.log("1");
+	}
+>>>>>>> origin/master
 	jQuery.ajax({
-        type : "GET",
-            async: true,
-            cache: false,
-            datatype : "json",
-            url : "/route/detail?routeId="+productId,
-            success : function(result){
-              if(result.status == "success"){
-                 $("#orderThumbnail").attr("src",result.data.thumbnail);
-                 $("#orderTitle").text(result.data.name);
-              }else{
-                alert(result.tipMsg);
-              }
-            },
-            error : function(data) {
-            	dealFailedResponse(result);
-            }
-      });
+		type : "GET",
+		async : true,
+		cache : false,
+		datatype : "json",
+		url : "/route/detail?routeId=" + productId,
+		success : function(result) {
+			if (result.status == "success") {
+				$("#orderThumbnail").attr("src", result.data.thumbnail);
+				$("#orderTitle").text(result.data.name);
+			} else {
+				dealFailedResponse(result);
+			}
+		},
+		error : function(data) {
+			dealFailedResponse(result);
+		}
+	});
 }
+
 
 
 //提交订单相关的游客信息
@@ -198,9 +257,10 @@ function initOrderInfo(){
           }
         }); 
 
-    }
-  
-  );
 
-          
 
+
+
+		}
+
+);
