f2fa46542941106c131326d8bfa4efa5a9f4588e
panpan040828 <875371217@qq.com>
2016-01-21 11:10:32 +0800

    个人中心页订单id一样的问题
    个人中心页订单id一样的问题

--- static/js/personalcenter/personalcenter.js
+++ static/js/personalcenter/personalcenter.js
@@ -4,39 +4,39 @@
     $(document).ready(function() { 
        initUserInfo();// 加载游客基本信息
             var paginationOptions = {
-             domId : "pager2",
-             url : "/routeOrder/getByPage",
-             page : 1,
-             rows : 4,
-             queryParams : {orderStatus:""},
-             dealData : function(dataList){                       
+                domId : "pager2",
+                url : "/routeOrder/getByPage",
+                page : 1,
+                rows : 6,
+                queryParams : {orderStatus:""},
+                dealData : function(dataList){
+                      for(var j=0; j<6; j++)
+                      {$("#order_"+j).css("display","none" );}                       
                       var len = dataList.length;
-                      //console.log(len);
-                       if(len > 0)
-                       {
+                      if(len > 0)
+                      {
                         for(var i=0; i<len; i++)
                         {
-                         var row = dataList[i];
-                                   $("#order_"+i).css("display","block" );                  
-                                   var href=$("#ordermore_"+i).attr("href");
-                                   console.log(href);
-                                  if(href.indexOf("routeOrderId")!=-1)
-                                   {
-                                    //console.log(href);
-                                    $("#ordermore_"+i).attr("href",href);                               
-                                   }
-                                   else
-                                  {
-                                    $("#ordermore_"+i).attr("href",href+"?"+"routeOrderId="+row.routeOrderId);
-                                  }                                 
+                            var row = dataList[i];
+                            $("#order_"+i).css("display","block" );                  
+                            var href=$("#ordermore_"+i).attr("href");
+                            
+                            if(href.indexOf("routeOrderId")!=-1)
+                            {
+                               $("#ordermore_"+i).attr("href",href);                               
+                            }
+                            else
+                            {
+                                $("#ordermore_"+i).attr("href",href+"?"+"routeOrderId="+row.routeOrderId);
+                            }                                 
                                             
-                                  $("#order_number_"+i).text(row.orderNo);
-                                  $("#order_thumbnail_"+i).attr("src", row.orderThumbnail);
-                                  $("#order_startTime_"+i).text(row.startTime);
-                                  $("#order_Title_"+i).text(row.orderTitle);
-                                  var orderStatus=parseInt(row.orderStatus);
-                                  switch(orderStatus)
-                                  {
+                            $("#order_number_"+i).text(row.orderNo);
+                            $("#order_thumbnail_"+i).attr("src", row.orderThumbnail);
+                            $("#order_startTime_"+i).text(row.startTime);
+                            $("#order_Title_"+i).text(row.orderTitle);
+                            var orderStatus=parseInt(row.orderStatus);
+                            switch(orderStatus)
+                            {
                                     case 01:
                                     $("#order_Status_"+i).text("未支付");
                                     break;
@@ -72,51 +72,62 @@
                                     case 09:
                                     $("#order_Status_"+i).text("退款中");
                                     break;
-                                  }
-                                    if(row.orderStatus!=01)
-                                      {$("#quickly_pay_"+i).addClass("disabled");
-                                     $("#quickly_delete_"+i).addClass("disabled");}
-                                    if(row.orderStatus!=03)
-                                      {$("#quickly_comment_"+i).addClass("disabled")}
-                                  console.log("/routeOrder/cancel?routeOrderId="+row.routeOrderId);
-                                  $("#quickly_delete_"+i).click(
-                                      function(){
-                                        jQuery.ajax({
-                                        type : "GET",
-                                        async: true,
-                                        cache: false,
-                                        datatype : "json",
-                                        url : "/routeOrder/cancel",
-                                        data:{routeOrderId:row.routeOrderId},
-                                        success : function(result){
-                                          if(result.status == "success"){ 
-                                          for(var i=0; i<6; i++)
-                                          {$("#route_"+i).css("display","none" );} 
-                                           pagination(paginationOptions);                                         
-                                            //$("#order_"+i).css("display","none");
-                                            //console.log("/routeOrder/cancel?routeOrderId="+row.routeOrderId);
-                                           //$("#passwordchange").attr("href","/personalcenter/personalinfo-main.html");                                                      
-                                         }else{
-                                           dealFailedResponse(result);
-                                         }
-                                       },
-                                       error : function(data) {
-                                         
-                                         alert("系统异常");
-                                       }
-                                     }); 
+                            }
+                            
+                            if(row.orderStatus!=01)
+                            {
+                              $("#quickly_pay_"+i).addClass("disabled");
+                              $("#quickly_delete_"+i).addClass("disabled");
+                            }
 
-                                   });
-                                  var maleCount=parseInt(row.maleCount);
-                                  var femaleCount=parseInt(row.femaleCount);
-                                  var price=parseInt(row.price);
-                                  $("#order_totalprice_"+i).text(price*(maleCount+femaleCount));
-                                  $("#order_totalcount_"+i).text(maleCount+femaleCount);
-                                  $("#order_createTime_"+i).text(row.createTime);
-                         }
+                            if(row.orderStatus!=03)
+                            {
+                              $("#quickly_comment_"+i).addClass("disabled");
+                            }
+                                  
+                            $("#quickly_delete_"+i).attr("value",row.routeOrderId);
+                            var maleCount=parseInt(row.maleCount);
+                            var femaleCount=parseInt(row.femaleCount);
+                            var price=parseInt(row.price);
+                            $("#order_totalprice_"+i).text(price*(maleCount+femaleCount));
+                            $("#order_totalcount_"+i).text(maleCount+femaleCount);
+                            $("#order_createTime_"+i).text(row.createTime);
                         }
-              }
-           }
+                      }
+                }
+            }
+
+           $(".quickly_delete").click(
+               function(){                                                                            
+                           var routeOrderId=$(this).attr("value");
+                           console.log(routeOrderId);
+                           jQuery.ajax({
+                                         type : "GET",
+                                         async: true,
+                                         cache: false,
+                                         datatype : "json",
+                                         url : "/routeOrder/cancel",
+                                         data:{routeOrderId:routeOrderId},
+                                         success : function(result){
+                                                   if(result.status == "success")
+                                                   { 
+                                                       for(var j=0; j<6; j++)
+                                                       {$("#order_"+j).css("display","none" );}     
+                                                       pagination(paginationOptions);                                     
+                                
+                                                   }
+                                                    else
+                                                    {
+                                                       dealFailedResponse(result);
+                                                    }
+                                         },
+                                         error : function(data) 
+                                         {                                        
+                                               alert("系统异常");
+                                         }
+                           }); 
+
+           });
                  pagination(paginationOptions); 
     });
 
