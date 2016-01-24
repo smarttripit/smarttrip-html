b1fa81d82d124df66914762a6da19e04e828511b
songjie <songjiesdnu@163.com>
2016-01-24 23:01:18 +0800

    修改微信支付

--- static/js/personalcenter/myorderlist-sub.js
+++ static/js/personalcenter/myorderlist-sub.js
@@ -5,7 +5,7 @@
     //var prepayId = "";
     var orderStatus = "";
     //alert("routeOrderId:" + routeOrderId);
-    alert("code:" + code);
+    //alert("code:" + code);
     //console.log(spotId);
     $(document).ready(function() {
     	initOrderInfo();// 加载订单详情
@@ -29,6 +29,7 @@
                 datatype : "json",
                 url : "/weixin/getH5Data?code="+code+"&routeOrderId="+routeOrderId,
                 success : function(result){
+                	alert("result.status=" + result.status);
                   if(result.status == "success"){
                     popWeixinPay(result.data);
                   }else{
@@ -55,9 +56,21 @@
     function popWeixinPay(data){
     	WeixinJSBridge.invoke(
 	       'getBrandWCPayRequest', 
-	       data,
+	       {
+	           "appId":data.appId,     //公众号名称，由商户传入     
+	           "timeStamp":data.timeStamp,         //时间戳，自1970年以来的秒数     
+	           "nonceStr":data.nonceStr, //随机串     
+	           "package":data.package,     
+	           "signType":data.signType,         //微信签名方式：     
+	           "paySign":data.paySign //微信签名 
+	       },
 	       function(res){     
-	           if(res.err_msg == "get_brand_wcpay_request:ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
+	    	   // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
+	           if(res.err_msg == "get_brand_wcpay_request:ok" ) {
+	        	   alert("支付成功");
+	           }else{
+	        	   alert(res.err_msg);
+	           } 
 	       }
 	   ); 
     }
