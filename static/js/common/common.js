b146326001752e6513aa1f12f352ce131d554ed5
songjie <songjiesdnu@163.com>
2016-01-27 21:21:19 +0800

    去掉正在加载中效果，使用盼盼的

--- static/js/common/common.js
+++ static/js/common/common.js
@@ -17,38 +17,6 @@ function dealFailedResponse(result) {
 	}
 }
 
-// 数据请求过程中的加载中效果-开始
-var loadingSpinOpts = {            
-    lines: 13, // 花瓣数目
-    length: 20, // 花瓣长度
-    width: 10, // 花瓣宽度
-    radius: 30, // 花瓣距中心半径
-    corners: 1, // 花瓣圆滑度 (0-1)
-    rotate: 0, // 花瓣旋转角度
-    direction: 1, // 花瓣旋转方向 1: 顺时针, -1: 逆时针
-    color: '#000', // 花瓣颜色
-    speed: 1, // 花瓣旋转速度
-    trail: 60, // 花瓣旋转时的拖影(百分比)
-    shadow: false, // 花瓣是否显示阴影
-    hwaccel: false, //spinner 是否启用硬件加速及高速旋转            
-    className: 'spinner', // spinner css 样式名称
-    zIndex: 2e9, // spinner的z轴 (默认是2000000000)
-    top: '50%', // spinner 相对父容器Top定位 单位 px
-    left: '50%'// spinner 相对父容器Left定位 单位 px
-};
-var loadingSpinner = new Spinner(loadingSpinOpts);
-// 弹出加载中蒙版
-function popLoading(){ 
-	var html = "<div style='position:absolute;height:"+$(document).height()+"px;width:100%;z-index:10000000;left:0;top:0;background-color:#000;opacity:0.1;'></div>";
-	$("body").append(html);
-	var target = $("body").get(0);
-	loadingSpinner.spin(target);   
-}
-// 关闭加载中蒙版
-function closeLoading(){
-	loadingSpinner.spin();
-}
-//数据请求过程中的加载中效果-开始
 // 根据url中的参数名称获取参数值
 (function($) {
 	$.getUrlParam = function(name) {
